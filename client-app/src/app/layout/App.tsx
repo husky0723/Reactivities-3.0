65143bdf72740f74fcee0728da5c13d73aec3a8a
Jello235 <73104351+Jello235@users.noreply.github.com>
2020-12-29 17:06:13 +0800

    Initial Commit

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, Fragment, useContext } from "react";
+import { Container } from "semantic-ui-react";
+import { NavBar } from "../../features/nav/NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { LoadingComponent } from "./LoadingComponent";
+import ActivityStore from "../stores/activityStore";
+import { Route } from "react-router-dom";
+import { HomePage } from "../../features/home/HomePage";
+import { ActivityForm } from "../../features/activities/form/ActivityForm";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
+
+const App = () => {
+  const activityStore = useContext(ActivityStore);
+
+  useEffect(() => {
+    activityStore.loadActivites();
+  }, [activityStore]);
+
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content="Loading activities..." />;
+
+  return (
+    <Fragment>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/activities' component={ActivityDashboard} />
+        <Route path='/activities/:id' component={ActivityDetails} />
+        <Route path='/createActivity' component={ActivityForm} />
+      </Container>
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,24 @@
+import { observer } from "mobx-react-lite";
+import React, { useContext } from "react";
+import { Grid } from "semantic-ui-react";
+import ActivityDetails from "../details/ActivityDetails";
+import { ActivityForm } from "../form/ActivityForm";
+import ActivityList from "./ActivityList";
+import ActivityStore from "../../../app/stores/activityStore";
+
+const ActivityDashboard: React.FC = () => {
+  const activityStore = useContext(ActivityStore);
+  const { editMode, activity } = activityStore;
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityList />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <h2>holders</h2>
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default observer(ActivityDashboard);
